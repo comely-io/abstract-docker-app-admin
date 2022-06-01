697982a11e7c349618335388988c49a6779ee30e
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-06-02 04:39:59 +0500

    updates

--- admin/src/app/auth/staff/edit-admin/edit-admin.component.ts
+++ admin/src/app/auth/staff/edit-admin/edit-admin.component.ts
@@ -4,6 +4,20 @@ import {AdminPanelService} from "../../../../services/adminPanelService";
 import {ActivatedRoute, Params} from "@angular/router";
 import {ApiQueryFail, ApiSuccess} from "../../../../services/apiService";
 import {staffMember} from "../list/list.component";
+import {FormControl, FormGroup} from "@angular/forms";
+import {ValidatorService} from "../../../../services/validatorService";
+
+
+interface permissionEntity {
+  name: string,
+  desc?: string | null | undefined,
+  current: boolean,
+  type: number
+}
+
+interface permissionChange {
+  [key: string]: boolean
+}
 
 @Component({
   selector: 'app-edit-admin',
@@ -13,14 +27,50 @@ import {staffMember} from "../list/list.component";
 export class EditAdminComponent implements OnInit {
   private staffId!: number;
   public staff!: staffMember;
+  public loadedPermissions?: Map<string, permissionEntity>;
+  public changePermissions: permissionChange = {};
+
+  public validator: ValidatorService;
+  public formsAreDisabled: boolean = true;
+  public editAccountSubmit: boolean = false;
+  public changePwSubmit: boolean = false;
+  public editPermSubmit: boolean = false;
+
+  public editAccountForm: FormGroup = new FormGroup({
+    status: new FormControl(),
+    email: new FormControl(),
+    totp: new FormControl()
+  });
+
+  public changePasswordForm: FormGroup = new FormGroup({
+    tempPassword: new FormControl(),
+    totp: new FormControl()
+  });
+
+  public editPermissionsForm: FormGroup = new FormGroup({
+    totp: new FormControl()
+  });
 
   constructor(private app: AppService, private aP: AdminPanelService, private route: ActivatedRoute) {
+    this.validator = app.validator;
     this.route.queryParams.subscribe((params: Params) => {
       this.staffId = parseInt(params["admin"]);
     });
   }
 
-  private async getStaff() {
+  public editAccountTotpType(e: any) {
+
+  }
+
+  public changePwTotpType(e: any) {
+
+  }
+
+  public editPermTotpType(e: any) {
+
+  }
+
+  private async loadStaffAccount() {
     if (!this.staffId) {
       this.app.router.navigate(["/auth/staff/list"]).then();
       return;
@@ -38,10 +88,67 @@ export class EditAdminComponent implements OnInit {
       this.app.router.navigate(["/auth/staff/list"]).then();
       return;
     }
+
+    this.formsAreDisabled = false;
+    this.editAccountForm.controls.status.setValue(this.staff.status ? "1" : "0");
+    this.editAccountForm.controls.email.setValue(this.staff.email);
+
+    this.loadPermissions().then();
+  }
+
+  private async loadPermissions() {
+    if (!this.staff) {
+      return;
+    }
+
+    if (this.staff.isRoot) {
+      return;
+    }
+
+    await this.app.api.callServer("get", "/auth/staff/privileges", {id: this.staff.id}).then((success: ApiSuccess) => {
+      if (success.result.hasOwnProperty("permissions") && typeof success.result.permissions === "object") {
+        this.loadedPermissions = success.result.permissions;
+        Object.keys(success.result.permissions).forEach((key: string) => {
+          this.changePermissions[key] = success.result.permissions[key].current ?? false;
+        });
+      }
+    }).catch((error: ApiQueryFail) => {
+      this.app.flash.staffRetrieveFail = error.error ?? error.exception?.message ?? undefined;
+    });
+
+    if (!this.loadedPermissions) {
+      this.app.router.navigate(["/auth/staff/list"]).then();
+      return;
+    }
+  }
+
+  public togglePermission(id: string): void {
+    this.changePermissions[id] = !(this.changePermissions.hasOwnProperty(id) && this.changePermissions[id]);
+    console.log(this.changePermissions);
+  }
+
+  public permissionLegend(type: number): string {
+    if (type === 2) {
+      return 'fal fa-exclamation-circle ms-2';
+    } else if (type === 1) {
+      return 'fal fa-exclamation-circle ms-2';
+    }
+
+    return '';
+  }
+
+  public permissionColor(type: number): string {
+    if (type === 2) {
+      return "text-danger";
+    } else if (type === 1) {
+      return "text-warning";
+    }
+
+    return "";
   }
 
   ngOnInit(): void {
-    this.getStaff().then();
+    this.loadStaffAccount().then();
 
     this.aP.breadcrumbs.next([
       {page: 'Staff Control', active: true},

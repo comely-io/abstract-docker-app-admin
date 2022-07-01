c5f118c5dfcae681aae7dba3e92766a2cc141520
Furqan Siddiqui <hello@furqansiddiqui.com>
2022-07-01 20:06:48 +0500

    editing

--- admin/src/app/auth/users/user-groups/edit-group/edit-group.component.ts
+++ admin/src/app/auth/users/user-groups/edit-group/edit-group.component.ts
@@ -1,10 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ValidatorService} from "../../../../../services/validatorService";
 import {FormControl, FormGroup} from "@angular/forms";
-import {AppService} from "../../../../../services/appService";
+import {ApiErrorHandleOpts, AppService} from "../../../../../services/appService";
 import {MdbModalRef} from "mdb-angular-ui-kit/modal";
 import {userGroup} from "../user-groups.component";
 import {group} from "@angular/animations";
+import {ApiQueryFail, ApiSuccess} from "../../../../../services/apiService";
 
 @Component({
   selector: 'app-edit-group',
@@ -30,7 +31,71 @@ export class EditGroupComponent implements OnInit {
   }
 
   public async submitEditGroupForm() {
+    let inputErrors: number = 0;
+    let groupId: number = 0,
+      name: string = "",
+      totp: string = "";
 
+    // Group ID
+    try {
+      groupId = parseInt(this.editGroupForm.get("group")?.value);
+      if (!(groupId > 0 && groupId < Number.MAX_SAFE_INTEGER)) {
+        throw new Error('Invalid selected users group to edit');
+      }
+    } catch (e) {
+      this.editGroupForm.get("group")?.setErrors({message: e.message});
+      inputErrors++;
+    }
+
+    // Name
+    try {
+      name = this.editGroupForm.get("name")?.value;
+      if (!name || !name.length) {
+        throw new Error('Group label is required');
+      }
+
+      if (!/^[a-z\-_.]+(\s[a-z\-_.]+)*$/i.test(name)) {
+        throw new Error('Invalid group name');
+      }
+
+      if (name.length < 3 || name.length > 32) {
+        throw new Error('Group name cannot exceed 32 bytes in length');
+      }
+    } catch (e) {
+      this.editGroupForm.get("name")?.setErrors({message: e.message});
+      inputErrors++;
+    }
+
+    // Totp
+    try {
+      totp = this.app.validator.validateTotp(this.editGroupForm.get("totp")?.value);
+    } catch (e) {
+      this.editGroupForm.get("totp")?.setErrors({message: e.message});
+      inputErrors++;
+    }
+
+    // Errors?
+    if (inputErrors !== 0) {
+      return;
+    }
+
+    // Clear out TOTP code
+    this.editGroupForm.get("totp")?.setValue("");
+
+    this.formIsLoading = true;
+    await this.app.api.callServer("post", "/auth/users/groups", {
+      group: groupId,
+      name: name,
+      totp: totp
+    }).then((success: ApiSuccess) => {
+      if (success.result.hasOwnProperty("status") && success.result.status === true) {
+        this.app.events.usersGroupReload().next(null);
+        this.modalRef.close();
+      }
+    }).catch((error: ApiQueryFail) => {
+      this.app.handleAPIError(error, <ApiErrorHandleOpts>{formGroup: this.editGroupForm});
+    })
+    this.formIsLoading = false;
   }
 
   public totpSubmit(e: any): void {
